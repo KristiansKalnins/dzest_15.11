4a09dc6077653e7784f6e4c1320f6e4e8b7a51fd
KristiansKalnins <kristians.kalnins06@grobinasskola.lv>
2023-11-15 10:51:17 +0200

    atrisināts pirmais uzdevums

---
+++ 1uzdevums.py
@@ -0,0 +1,16 @@
+"""
+izveidot programmu, kura prasa lietotājam ievadīt divus skaitļus un aprēķina to summu
+izvadot terminālī
+"""
+ 
+
+
+
+a=int(input("ievadi pirmo skaitli"))
+b=int(input("Ievadi otro skaitli"))
+c = a + b
+print(f"a + b = {c}")
+
+
+
+# atrisināts pirmais uzdevums
